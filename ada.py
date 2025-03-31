314234f9c19648f49610a56b03faf5cf073a16ab
Rozas Agustin <107058124+Skintblock@users.noreply.github.com>
2025-03-31 12:17:53 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,11 @@
+#31/3/2025
+#TP2 - Rozas Miguel Agustin - Introduccion a la programacion 1
+#Ejercicio 1
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    full_name = first_name + " " + last_name
+    print (full_name.lower())
+    print (full_name.title())
+    print (full_name.upper())
+    print ("\t" + full_name.lower())
