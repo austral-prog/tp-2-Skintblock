a67e4c0f601857b364aad9bb2ff9a567c17167db
Rozas Agustin <107058124+Skintblock@users.noreply.github.com>
2025-03-31 12:19:32 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,17 @@
+#31/3/2025
+#TP2 - Rozas Miguel Agustin - Introduccion a la programacion 1
+#Ejercicio 3
 def change():
     expense = 23.75
     money = 100
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+    print("")
+    print("Vuelto")
+    print("")
+    print("Pesos:")
+    print(int(money - expense))
+    print("Centavos:")
+    print(int((money - expense) * 100 % 100))
